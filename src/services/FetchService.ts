0d8af31e3aba38191764ef6c53209a2d3e7d2f79
Andrei <dre021088@gmail.com>
2023-11-17 22:23:09 +0700

    cleaning

--- src/services/FetchService.ts
+++ src/services/FetchService.ts
@@ -12,16 +12,9 @@ const api = Axios.create(settings);
 
 const FetchService = {
    async getList(searchStr: string) {
-      await _wait_(1);
       const query = searchStr ? `?q=${searchStr}` : '';
       return await api.get<IPost[]>(`/posts${query}`);
    }
 };
 
 export default FetchService;
-
-export async function _wait_(seconds = 3) {
-   return new Promise((resolve) => {
-      setTimeout(() => resolve(true), seconds * 1000);
-   });
-}
